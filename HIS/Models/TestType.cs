0700b629693d9fcdd7a21288c076abc960000e57
satmca2011 <40752758+satmca2011@users.noreply.github.com>
2018-11-15 21:37:36 +0530

    Test types commit

---
+++ HIS/Models/TestType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HIS
+{
+    public partial class TestType
+    {
+    }
+    public class LabKitViewModel : TestType
+    {
+        public string LKitName { get; set; }
+        public string InputTest { get; set; }
+        public decimal LKitCost { get; set; }
+        public int LKitID { get; set; }
+    }
+}
